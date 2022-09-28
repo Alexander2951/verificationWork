73e17738f6c69cca7818fbcdab9ab73a63fceb83
Alexander <alex-lama@yandex.ru>
2022-09-29 00:34:17 +0300

    организовал загрузку массива с клавиатуры

---
+++ verificationTask/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество строк массива");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] text = new string [n];
+Console.WriteLine("Вводите строки нажимая 'Enter' после каждой строки");
+for (int i = 0; i < n; i++)
+{
+    text[i] = Convert.ToString(Console.ReadLine());
+}
+Console.WriteLine(text[2]);
