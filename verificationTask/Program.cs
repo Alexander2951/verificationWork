f9a04d5ff06f0027732c798e1a812595617d72a8
Alexander <alex-lama@yandex.ru>
2022-10-03 19:53:54 +0300

    эээ... что то я забыл...

--- verificationTask/Program.cs
+++ verificationTask/Program.cs
@@ -6,4 +6,22 @@ for (int i = 0; i < n; i++)
 {
     text[i] = Convert.ToString(Console.ReadLine());
 }
-Console.WriteLine(text[2]);
+int k=0;
+for (int i = 0; i < n; i++)
+{
+    if  (text[i].Length <=3 )
+    {
+        k++;
+    }
+}
+string[] text2 = new string [k];
+int l=0;
+for (int i = 0; i < n; i++)
+{
+    if (text [i].Length <=3)
+    {
+        text2[l] = text[i];
+        l++;
+    }
+}
+Console.WriteLine (text);
